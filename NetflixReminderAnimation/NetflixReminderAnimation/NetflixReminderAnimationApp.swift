177be128d486971bcc3f36f8606d48ccb30487c4
Anup DSouza <anup.dsouza@gmail.com>
2023-12-06 12:15:21 +0530

    Add initial files

---
+++ NetflixReminderAnimation/NetflixReminderAnimation/NetflixReminderAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixReminderAnimationApp.swift
+//  NetflixReminderAnimation
+//
+//  Created by Anup D'Souza on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NetflixReminderAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
